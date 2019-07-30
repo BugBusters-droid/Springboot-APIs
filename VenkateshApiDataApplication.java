463832844f920744560e2fc84250e2bbc3ec441a
BugBusters-droid <53464808+BugBusters-droid@users.noreply.github.com>
2019-07-30 12:31:46 +0530

    Add files via upload

---
+++ VenkateshApiDataApplication.java
@@ -0,0 +1,13 @@
+package io.venkatesh.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenkateshApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenkateshApiDataApplication.class, args);
+	}
+
+}
